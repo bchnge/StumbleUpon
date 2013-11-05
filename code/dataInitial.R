96faa35b6ce7d85c5f28b26930db6f178d7afbd9
Ben Cheng <ben.y.cheng@gmail.com>
2013-11-05 18:01:19 -0500

    addtional submissions. Testing basic RF models

--- code/dataInitial.R
+++ code/dataInitial.R
@@ -3,6 +3,9 @@ setwd('~/Projects/StumbleUpon/')
 dfTrain <- read.csv('train.tsv',sep='\t',stringsAsFactors=FALSE)
 dfTest <- read.csv('test.tsv', sep = '\t',stringsAsFactors=FALSE)
 
+require(Boruta)
+
+
 logit_1 <- glm(label ~ html_ratio + image_ratio + embed_ratio + is_news, data = dfTrain, family = "binomial")
 
 summary(logit_1)
@@ -14,3 +17,101 @@ dfSubmission <- dfTest[, grep('urlid|label',names(dfTest))]
 
 write.table(dfSubmission,file="submissions/submission_1.csv",
             sep=",",row.names=F)         
+
+
+#############################################################
+
+dfTrain <- read.csv('train.tsv',sep='\t',stringsAsFactors=F)
+dfTest <- read.csv('test.tsv', sep = '\t',stringsAsFactors=F)
+
+require(dummies)
+
+dfTrain$alchemy_category[dfTrain$alchemy_category == 'unknown' | dfTrain$alchemy_category == 'weather'] <- '?'
+
+
+dfTrain2 <- dummy.data.frame(dfTrain,names = c('alchemy_category', 'is_news', 'news_front_page'))
+dfTest2 <- dummy.data.frame(dfTest, names = c('alchemy_category', 'is_news', 'news_front_page'))
+dfTrain2$label <- factor(dfTrain2$label)
+dfTrain2 <- dfTrain2[, names(dfTrain2) != 'alchemy_category_score']
+dfTest2 <- dfTest2[, names(dfTest2) != 'alchemy_category_score']
+
+
+rf <- randomForest(dfTrain2[,4:39], dfTrain2[,40])
+a <- predict(rf, newdata = dfTest2, type = "prob")
+dfTest$label <- a[,2]
+dfSubmission <- dfTest[, grep('urlid|label',names(dfTest))]
+
+write.table(dfSubmission,file="submissions/submission_3.csv",
+            sep=",",row.names=F)         
+
+#################################################################################
+
+
+dfTrain <- read.csv('train.tsv',sep='\t',stringsAsFactors=F)
+dfTest <- read.csv('test.tsv', sep = '\t',stringsAsFactors=F)
+
+require(dummies)
+
+dfTrain$alchemy_category[dfTrain$alchemy_category == 'unknown' | dfTrain$alchemy_category == 'weather'] <- '?'
+
+
+dfTrain2 <- dummy.data.frame(dfTrain,names = c('alchemy_category', 'is_news', 'news_front_page'))
+dfTest2 <- dummy.data.frame(dfTest, names = c('alchemy_category', 'is_news', 'news_front_page'))
+dfTrain2$label <- factor(dfTrain2$label)
+dfTrain2 <- dfTrain2[, names(dfTrain2) != 'alchemy_category_score']
+dfTest2 <- dfTest2[, names(dfTest2) != 'alchemy_category_score']
+
+require(Boruta)
+
+
+boruta <- Boruta(dfTrain2[,4:39], dfTrain2[,40])
+
+# drop list from Boruta run
+dropList <- c('alchemy_categorygaming','alchemy_categorylaw_crime','alchemy_categoryreligion','alchemy_categoryscience_technology','framebased')
+
+x <- dfTrain2[,4:39]
+x <- x[,!(names(x) %in% dropList)]
+y <- dfTrain2[,40]
+
+rf <- randomForest(x, y)
+a <- predict(rf, newdata = dfTest2, type = "prob")
+dfTest$label <- a[,2]
+dfSubmission <- dfTest[, grep('urlid|label',names(dfTest))]
+
+write.table(dfSubmission,file="submissions/submission_4.csv",
+            sep=",",row.names=F)         
+
+#####################################
+
+# Determine Number of Factors to Extract
+library(nFactors)
+ev <- eigen(cor(x_num)) # get eigenvalues
+ap <- parallel(subject=nrow(x_num),var=ncol(x_num),
+               rep=100,cent=.05)
+nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
+plotnScree(nS)
+
+
+# Maximum Likelihood Factor Analysis
+# entering raw data and extracting 3 factors, 
+# with varimax rotation 
+dropList2 <- names(x)[grep('alchemy_*|is_news*|news_front_page*',names(x))]
+x_num <- x[,!(names(x) %in% dropList2)]
+
+fit <- factanal(x_num, 6, rotation="varimax", scores = 'regression')
+x2 <- cbind(x, fit$scores)
+rf <- randomForest(x2, y)
+a <- predict(rf, newdata = dfTest2, type = "prob")
+dfTest$label <- a[,2]
+dfSubmission <- dfTest[, grep('urlid|label',names(dfTest))]
+
+write.table(dfSubmission,file="submissions/submission_4.csv",
+            sep=",",row.names=F)         
+
+
+
+print(fit, digits=2, cutoff=.3, sort=TRUE)
+# plot factor 1 by factor 2 
+load <- fit$loadings[,1:6] 
+plot(load,type="n") # set up plot 
+text(load,labels=names(x_num),cex=.7) # add variable names
--- code/dataViz.R
+++ code/dataViz.R
@@ -1,18 +1,156 @@
+setwd('~/Projects/StumbleUpon/')
+
 require(ggplot2)
 require(plyr)
 
-dfTrain$alchemy_category_score <- as.numeric(dfTrain$alchemy_category_score)
+dfTrain <- read.table(file = 'train.tsv', sep = '\t', header=T,stringsAsFactors=F)
 
-test = ddply(.data = dfTrain, .variables=c('alchemy_category'),
-             summarize,
-             avg_alch_score = mean(alchemy_category_score, na.rm=F)
-)
+require(tm)
+require(Snowball)
+require(wordcloud)
 
-cat_eg = ddply(.data = dfTrain, .variables=c('alchemy_category'),
-             summarize,
-             evergreen = mean(label, na.rm=F),
-             freq = length(alchemy_category)
-)
+### WORD CLOUDS
 
-write.table(cat_eg, file = 'cat_eg.tsv', sep = '\t', row.names = F)
+corpus2dtm <- function(df){
+  corpus <- Corpus(VectorSource(df$boilerplate))
+  corpus <- tm_map(corpus, tolower)
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, removeNumbers)
+  stopWords <- c(stopwords('SMART'),'title','url')
+  corpus <- tm_map(corpus, removeWords, stopWords)
+  corpus <- tm_map(corpus, stemDocument)  
+  dtm <- DocumentTermMatrix(corpus, control = list(minWordLength = 1))
+}
 
+cloudComparer<- function(topic){
+  dtm_eg <- corpus2dtm(dfTrain[dfTrain$alchemy_category == topic & dfTrain$label == 1, ])
+  dtm_neg <- corpus2dtm(dfTrain[dfTrain$alchemy_category == topic & dfTrain$label == 0, ])
+  
+  eg = colSums(as.matrix(dtm_eg))  
+  neg = colSums(as.matrix(dtm_neg))
+  
+  dfEG = data.frame(word = names(eg), EG = eg) 
+  dfNEG = data.frame(word = names(neg), NEG = neg) 
+  merged = merge(dfEG, dfNEG, by = 'word', all.x = T, all.y = T)
+  merged[is.na(merged)] <- 1
+  merged$Evergreen <- with(merged, EG/NEG)
+  merged$Ephem <- with(merged, NEG/EG)  
+  merged
+}
+
+require(RColorBrewer)
+pal <- brewer.pal(9,'Greens')  
+
+totalCloud<- function(){
+  
+  dtm_eg <- corpus2dtm(dfTrain[dfTrain$label == 1, ])
+  dtm_neg <- corpus2dtm(dfTrain[dfTrain$label == 0, ])
+  
+  eg = colSums(as.matrix(dtm_eg))  
+  neg = colSums(as.matrix(dtm_neg))
+  
+  dfEG = data.frame(word = names(eg), EG = eg) 
+  dfNEG = data.frame(word = names(neg), NEG = neg) 
+  merged = merge(dfEG, dfNEG, by = 'word', all.x = T, all.y = T)
+  merged[is.na(merged)] <- 1
+  merged$Evergreen <- with(merged, EG/NEG)
+  merged$Ephem <- with(merged, NEG/EG)  
+  merged
+}
+
+evergreenPlotter <- function(topic, maxWords = 30){
+  pal <- brewer.pal(9,'Greens')  
+  a <- cloudComparer(topic)
+  wordcloud(words = a$word, freq = a$Evergreen, max.words = maxWords, colors = pal)  
+  wordcloud(words = a$word, freq = a$Ephem, max.words = maxWords, colors = pal)  
+  
+  }
+
+ephemeralPlotter <- function(topic, maxWords = 30){
+  pal <- revbrewer.pal(9,'Greys')  
+  a <- cloudComparer(topic)
+  wordcloud(words = a$word, freq = a$Ephem, max.words = maxWords, colors = pal)  
+}
+
+
+pal_total1 <- brewer.pal(9, 'Greys')
+pal_total2 <- brewer.pal(9, 'Greens')
+
+total <- totalCloud()
+
+png('total.png',height = 1200, width = 1600)
+par(mfrow = c(1,2))
+wordcloud(words = total$word, freq = total$Evergreen, max.words = 20, colors = pal_total2)
+wordcloud(words = total$word, freq = total$Ephem, max.words = 20, colors = pal_total1)
+dev.off()
+
+
+allPlotter <- function(topic, maxWords = 30){
+  pal <- brewer.pal(9,'Greens')  
+  pal2 <- brewer.pal(9,'Greys')
+  a <- cloudComparer(topic)
+  wordcloud(words = a$word, freq = a$Evergreen, max.words = maxWords, colors = pal)  
+  wordcloud(words = a$word, freq = a$Ephem, max.words = maxWords, colors = pal2)  
+}
+
+png('arts_entertainment_wcloud.png',height = 800, width = 600)
+par(mfrow = c(1,2))
+allPlotter('arts_entertainment')
+dev.off()
+
+png('business_wcloud.png',height = 800, width = 600)
+par(mfrow = c(1,2))
+allPlotter('business')
+dev.off()
+
+png('unlabeled.png',height = 600, width = 600)
+par(mfrow = c(1,2))
+allPlotter('?')
+dev.off()
+
+png('recreation.png',height = 800, width = 600)
+par(mfrow = c(1,2))
+allPlotter('recreation')
+dev.off()
+
+
+
+
+
+#### summary bars ####
+require(plyr)
+catSet <- ddply(.data = dfTrain, .variables=c('alchemy_category'), summarize,
+                numWebsites = NROW(label),
+                avgUrlWords = mean(numwords_in_url),
+                Evergreen = 100*mean(label)
+                )
+
+require(ggplot2)
+require(scales)
+require(ggthemes)
+
+
+ggplot(catSet, aes(x = reorder(alchemy_category, numWebsites), y = numWebsites, fill = Evergreen)) + 
+  geom_bar(stat = 'identity') +  scale_fill_gradient2(low='white', high = 'darkgreen') +
+  xlab('Alchemy Category') +
+  ylab('Number of websites') +
+  theme_tufte() +
+  theme(axis.text.x = element_text(face = 'bold', size = 15, angle = 45, vjust = 0.5))
+
+ggplot(catSet, aes(x = avgUrlWords, y = numWebsites, fill = Evergreen)) + 
+  geom_point(aes(size = numWebsites), size = 10) +  scale_fill_gradient(low='white', high = 'forestgreen') +
+  theme_tufte()
+
+png('bubbles.png',height=1280,width=1920, res=100)
+qplot(x = alchemy_category, y = avgUrlWords, data = catSet, colour = Evergreen, size = numWebsites) + 
+  scale_color_gradient(low = 'white', high = 'darkgreen') + scale_size(range = c(10,40)) +
+  theme_tufte()
+dev.off()
+
+
+ggplot(dfTrain, aes(x = "", y = )) + geom_bar(width = 1) +
+  coord_polar(theta = 'y')
+
+
+symbols(x = catSet$avgUrlWords, y = catSet$Evergreen, circles = catSet$numWebsites/10)
+  
